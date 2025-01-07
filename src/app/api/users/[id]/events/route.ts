4cee1097d62f61029cc9272b3b323d38634e2c9d
JamesRobert20 <jmtendamema@gmail.com>
2025-01-07 11:48:44 -0700

    Commented unused variables
    Signed-off-by: JamesRobert20 <jmtendamema@gmail.com>

--- src/app/api/users/[id]/events/route.ts
+++ src/app/api/users/[id]/events/route.ts
@@ -6,11 +6,11 @@ export const dynamic = 'force-dynamic'
 export async function GET(req: Request) {
     try {
         const { searchParams } = new URL(req.url);
-        const searchTerm = searchParams.get('searchTerm') ?? "";
+        //const searchTerm = searchParams.get('searchTerm') ?? "";
 
-        const page = searchParams.get('page') ? Number(searchParams.get('page')) : 0;
-        const limit = searchParams.get('limit') ? Number(searchParams.get('limit')) : 10;
-        const sortParam = searchParams.get('sortParam');
+        //const page = searchParams.get('page') ? Number(searchParams.get('page')) : 0;
+        //const limit = searchParams.get('limit') ? Number(searchParams.get('limit')) : 10;
+        //const sortParam = searchParams.get('sortParam');
 
         // TODO: Add event helper function to get events that a user is attending
         const events: EventSchemaType[] = []
