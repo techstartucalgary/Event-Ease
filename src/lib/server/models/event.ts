72f0bba2479eb81c5639e234c779a945ddb4ce3e
yotam1802 <Yotam1802@gmail.com>
2025-01-08 14:50:33 -0700

    Update event model and type

--- src/lib/server/models/event.ts
+++ src/lib/server/models/event.ts
@@ -1,28 +1,99 @@
 import { Model, Schema } from "mongoose";
-import { EventSchemaType } from "@/lib/types/event";
+import {
+  BulkEventDataToUpdate,
+  EventSchemaType,
+  NewEvent,
+} from "@/lib/types/event";
 import { entityTypes } from "@/lib/helpers";
 import { DATABASE_CONNECTION } from "@/lib/server/helpers/database";
+import * as yup from "yup";
+import { getStringSchema } from "@/lib/helpers";
 
-const EventSchema = new Schema<EventSchemaType>({
+const EventSchema = new Schema<EventSchemaType>(
+  {
     name: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     description: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     creator: {
-        type: String,
-        required: true,
-        refPath: "creatorType"
+      type: String,
+      required: true,
+      refPath: "creatorType",
     },
     creatorType: {
-        type: String,
-        required: true,
-        enum: entityTypes
-    }
-}, { collection: "Events", timestamps: true, versionKey: false })
+      type: String,
+      required: true,
+      enum: entityTypes,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    participants: {
+      type: [
+        {
+          type: String,
+          ref: "User",
+        },
+      ],
+      required: false,
+      default: [],
+    },
+    status: {
+      type: String,
+      enum: ["Upcoming", "Ongoing", "Completed", "Cancelled"],
+      required: true,
+      default: "Upcoming",
+    },
+  },
+  { collection: "Events", timestamps: true, versionKey: false }
+);
 
-export const EventModel = (DATABASE_CONNECTION.models.Event as Model<EventSchemaType>)
-    || DATABASE_CONNECTION.model<EventSchemaType>("Event", EventSchema)
+export function validateNewEventData(params: NewEvent) {
+  // Validate the provided params for creating a new event
+  const { name, description, creator, creatorType, location } = params;
+
+  const schema = yup.object().shape({
+    name: getStringSchema({ message: "Event name is required" }),
+    description: getStringSchema({ message: "Event description is required" }),
+    creator: getStringSchema({ message: "Creator ID is required" }),
+    creatorType: yup
+      .string()
+      .oneOf(entityTypes, "Invalid creator type")
+      .required("Creator type is required"),
+    location: getStringSchema({ message: "Location is required" }),
+  });
+
+  return schema.validateSync({
+    name,
+    description,
+    creator,
+    creatorType,
+    location,
+  }) as NewEvent;
+}
+
+export function validateEventUpdates(data: BulkEventDataToUpdate) {
+  // Validate the provided params for updating an event
+  const schema = yup.object().shape({
+    name: getStringSchema({ required: false }),
+    description: getStringSchema({ required: false }),
+    location: getStringSchema({ required: false }),
+    status: yup
+      .string()
+      .oneOf(
+        ["Upcoming", "Ongoing", "Completed", "Cancelled"],
+        "Invalid status"
+      ),
+  });
+
+  return schema.validateSync(data, { stripUnknown: true });
+}
+
+export const EventModel =
+  (DATABASE_CONNECTION.models.Event as Model<EventSchemaType>) ||
+  DATABASE_CONNECTION.model<EventSchemaType>("Event", EventSchema);
--- src/lib/types/event.ts
+++ src/lib/types/event.ts
@@ -1,12 +1,20 @@
-import { BaseModel, EntityType } from "."
+import { BaseModel, EntityType } from ".";
 
 export type NewEvent = {
-    name: string,
-    description: string,
-    creator: string,
-    creatorType: EntityType
-}
+  name: string;
+  description: string;
+  creator: string;
+  creatorType: EntityType;
+  location: string;
+  participants: string[];
+  status: "Upcoming" | "Ongoing" | "Completed" | "Cancelled";
+};
 
-export type EventUpdateType = object;
+export type BulkEventDataToUpdate = {
+  name?: string;
+  description?: string;
+  location?: string;
+  status?: "Upcoming" | "Ongoing" | "Completed" | "Cancelled";
+};
 
 export type EventSchemaType = BaseModel & NewEvent;
