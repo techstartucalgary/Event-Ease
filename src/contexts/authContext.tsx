4b8d8092f8af7e313264f7eadb5f458afb331682
James Mtendamema <jmtendamema@gmail.com>
2025-02-12 19:08:21 -0700

    added user type

--- src/contexts/authContext.tsx
+++ src/contexts/authContext.tsx
@@ -1,15 +1,28 @@
 "use client"
 
 import { createContext, PropsWithChildren } from "react"
-import { ClerkProvider } from "@clerk/nextjs"
+import { ClerkProvider, useUser } from "@clerk/nextjs"
+import { prefixWithCloudUrl } from "@/lib/helpers";
+import { ClerkUser } from "@/lib/types/user";
 
 type authContext = {
+    user: ClerkUser | null;
 }
 const AuthContext = createContext<authContext | null>(null)
 
 function Provider({ children }: PropsWithChildren) {
+    const { user: clerkUser } = useUser();
+
+    const user = clerkUser ? {
+        id: clerkUser.id,
+        email: clerkUser.emailAddresses[0].emailAddress,
+        picture: !!clerkUser.publicMetadata.picture ? prefixWithCloudUrl("Users", clerkUser.publicMetadata.picture as string) : undefined,
+        fullName: clerkUser.fullName!,
+        username: clerkUser.username!
+    } : null
+
     return (
-        <AuthContext.Provider value={{}}>
+        <AuthContext.Provider value={{ user }}>
             {children}
         </AuthContext.Provider>
     )
